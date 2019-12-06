a9e659bdf350fcbb2f1b2bc31d1abb996c93e22e
mmulleri <58371218+mmulleri@users.noreply.github.com>
2019-12-05 21:27:05 -0500

    removed comments

--- JKMS/RI_portfolio_dyn.m
+++ JKMS/RI_portfolio_dyn.m
@@ -1,8 +1,8 @@
 function RI_portfolio_dyn(combo,yN,ncores)
 RI_portfolio_init
 mypool=parpool('local', ncores);
-%% grant access to relevant variables
-stategrid=stategrid; 
+%% grant parallel workers access to relevant variables
+stategrid=stategrid;
 u=u;
 lambda=lambda;
 b_logscales=b_logscales;
@@ -10,10 +10,10 @@ b_mat=b_mat;
 u_mat=u_mat;
 
 %% computation parameters
-K=10000; % number of actions considered simultaneously
+K=1000; % number of actions considered simultaneously
 p_consider=0.99; % consideration set that determines stabilization
-start_resolution=10; %initial actiongrid has 2^start_resolution+1 gridpoints in each dimension
-final_resolution=10; %final actiongrid has 2^final_resolution+1 gridpoints in each dimension
+start_resolution=3; %initial actiongrid has 2^start_resolution+1 gridpoints in each dimension
+final_resolution=9; %final actiongrid has 2^final_resolution+1 gridpoints in each dimension
 max_rounds=10; % maximum number of subroutines per resolution level
 min_prob=1e-4; % proportionality factor from minimal to maximal marginal
 maxz_tol=1e-15;
@@ -31,14 +31,12 @@ for r=start_resolution:final_resolution
         actiongrid_old=actiongrid;
         psi=prior./(b_mat*p_marg);
         z = @(a) psi'*exp(u(a,stategrid)/lambda + b_logscales)-1;
-        
+
         xticks=linspace(xrange_FI(1),xrange_FI(2),2^r+1)';
         actiongrid_new=generateactiongrid(xticks);
-%         % consider only actions that are not dominated
-%         actiongrid_new=setdiff(actiongrid_new,dominated_actions,'rows');
         % consider only actions that are not already in the old grid
         actiongrid_new=setdiff(actiongrid_new,actiongrid_old,'rows');
-        
+
         % compute z scores:
         b=b_mat*p_marg;
         psi=prior./b;
@@ -53,7 +51,7 @@ for r=start_resolution:final_resolution
         delta = -Delta * p_consider/(1-p_consider);
         actiongrid_add=actiongrid_new(zs>=delta,:);
         z_add=zs(zs>=delta);
-        
+
         if (size(setdiff(actiongrid_add,actiongrid_untruncated,'rows'),1)==0)
             fprintf('Resolution level %2i, round %2i:\n - Currently using %i actions.\n - Full grid adds %i actions.\n - Total cardinality of %2.2f%% cover is %i.\n - No additional actions are within the %2.2f%% cover.\n',...
                 r,i,size(actiongrid_old,1),size(actiongrid_new,1),p_consider*100,size(actiongrid_add,1)+size(actiongrid_old,1),p_consider*100);
@@ -61,7 +59,7 @@ for r=start_resolution:final_resolution
             stable=true;
             continue
         end
-        
+
         [~,usea] = maxk(z_add,K-size(actiongrid_old,1));
         actiongrid=[actiongrid_old;actiongrid_add(usea,:)];
         fprintf('Resolution level %2i, round %2i:\n - Currently using %i actions.\n - Full grid adds %i actions.\n - Total cardinality of %2.2f%% cover is %i.\n - Limiting menu to the %i existing actions and the %i actions with highest score.\n',...
@@ -71,31 +69,9 @@ for r=start_resolution:final_resolution
         [p_marg,~,~,exitflag,info] = GAP_SQP(u_mat,prior,lambda,'display','iter','actionlabels',actiongrid(:,2:3),'initial_p',p_marg);
         b_mat=info.b_mat;
         b_logscales=info.b_logscales;
-%         % determine if any actions are interior to the convex hull spanned
-%         % by b_mat
-%         I = size(b_mat,1);
-%         J = size(b_mat,2);
-%         psi=optimvar('psi',I,'LowerBound',0);
-%         prob_psi = optimproblem();
-%         prob_psi.Constraints.ineq = (b_mat'*psi <= ones(J,1));
-%         Jtot = size(actiongrid_new,1);
-%         Z0 = zeros(Jtot,1);
-%         parfor j=1:Jtot
-%             prob_psi_j=prob_psi;
-%             uj = u(actiongrid_new(j,:),stategrid);
-%             bj = exp(uj/lambda + b_logscales);
-%             prob_psi_j.Objective= -bj'*psi;
-%             maxzj = prob2struct(prob_psi_j);
-%             maxzj.options=optimset('Display',"none");
-%             [psizj,zj,found] = linprog(maxzj);
-%             if found==1
-%                 Z0(j) = -zj-1;
-%             end
-%         end
-%         dominated = (Z0<-1e-12);          
-%         fprintf(' - %i actions are interior to the learning-proof menu.\n',sum(dominated));
-%         dominated_actions = [dominated_actions; actiongrid_new(dominated,:)];
-        
+
+        save(sprintf('portfolio_solutions/portfolio_%s_Y%i_it%02i%02i_untrimmed.mat',combo,yN,r,i),'p_marg','actiongrid','stategrid','prior');
+
         % drop actions with small probability
         keep = any(p_marg>min_prob*max(p_marg),2);
         plotgrid=actiongrid;
@@ -105,9 +81,9 @@ for r=start_resolution:final_resolution
         u_mat(:,~keep)=[];
         p_marg(~keep)=[]; p_marg = p_marg/sum(p_marg); %trim marginals
         GAP_printmarg(p_marg,'actionlabels',actiongrid(:,2:3));
-        
-        save(sprintf('portfolio_solutions/portfolio_%s_Y%i_it%02i%02i.mat',combo,yN,r,i),'p_marg','actiongrid','stategrid','prior');
-        
+
+        save(sprintf('portfolio_solutions/portfolio_%s_Y%i_it%02i%02i_trimmed.mat',combo,yN,r,i),'p_marg','actiongrid','stategrid','prior');
+
         fig=figure('PaperPosition',[0,0,8,8],'PaperSize',[8,8]);
         clf
         scatter(plotgrid(:,2),plotgrid(:,3),3,'r','filled')
@@ -120,7 +96,7 @@ for r=start_resolution:final_resolution
         legend('RIsolve grid','RIsolve marginals');
         print(fig,'-dpdf',sprintf('portfolio_figs/%s_marginals_yN%i_it%02i%02i.pdf',combo,yN,r,i));
         close
-        
+
         i=i+1;
         if i>max_rounds
             fprintf('Resolution level %2i reached maximum number of subroutines before the %2.2f%% cover stabilized.\n',...
--- JKMS/RI_portfolio_plots.m
+++ JKMS/RI_portfolio_plots.m
@@ -31,7 +31,7 @@ parfor i=1:size(actiongrid,1)
             zsi=exp(ui/lambda + b_logscales)'*psi-one;
             zs(i)=zsi;
 end
-Delta = max(zs);
+Delta = max(zs)
 p_cover=[0.999,.99,.95];
 delta = -Delta * p_cover./(1-p_cover);
 covers=(zs>=delta);
@@ -41,14 +41,14 @@ fig=figure('PaperPosition',[0,0,3,3],'PaperSize',[3,3]);
 gray=0.8;
 clf
 hold on
-scatter(actiongrid(covers(:,1)==1,2),actiongrid(covers(:,1)==1,3),5,gray*[1 1 1],'filled')
+scatter(actiongrid(covers(:,2)==1,2),actiongrid(covers(:,2)==1,3),5,gray*[1 1 1],'filled')
 scatter(actiongrid(covers(:,3)==1,2),actiongrid(covers(:,3)==1,3),5,'k','filled')
 hold off
 ylim(plotrange);
 xlim(plotrange);
 xlabel('\theta_1');
 ylabel('\theta_2');
-legend('99.9% cover','95.0% cover','Location',[0.5 0.5 0 0]);
+legend('99% cover','95% cover','Location',[0.5 0.5 0 0]);
 print(fig,'-dpdf',sprintf('portfolio_figs/%s_final_bounds_%i.pdf',combo,yN));
 close
 
@@ -64,6 +64,32 @@ xlabel('\theta_1');
 ylabel('\theta_2');
 legend('JKMS','GAP-SQP','Location',[0.5 0.5 0 0]);
 print(fig,'-dpdf',sprintf('portfolio_figs/%s_final_marginals2D_%i.pdf',combo,yN));
+close
+
+umin=-1;
+
+n=size(stategrid,1);
+
+grid=linspace(umin,0,200);
+[~,weightsGAP,~,IGAP] = GAP_components(p_marg,u_mat,lambda,prior);
+[pdfGAP,uGAP]=ksdensity(u_mat(:)-lambda*IGAP,grid,'Weights',weightsGAP(:));
+[~,weightsJKMS,~,IJKMS] = GAP_components(p_marg_sims,u_mat_sims,lambda,prior);
+[pdfJKMS,uJKMS]=ksdensity(u_mat_sims(:)-lambda*IJKMS,grid,'Weights',weightsJKMS(:));
+[~,weightsJKMS,~,IJKMS] = GAP_components(p_marg_sims,u_mat_sims,lambda,prior);
+[pdfJKMS,uJKMS]=ksdensity(u_mat_sims(:)-lambda*IJKMS,grid,'Weights',weightsJKMS(:));
+[pdfFI,uFI]=ksdensity(max(u_mat,[],2),grid,'Weights',prior);
+[pdfNI,uNI]=ksdensity(u(NI_action,stategrid),grid,'Weights',prior);
+IE = @(p,b_mat) lambda*(log(b_mat*p)-b_logscales);
+[pdfIE,uIE]=ksdensity(IE(p_marg,b_mat),grid,'Weights',prior);
+
+fig=figure('PaperPosition',[0,0,8,4],'PaperSize',[8,4]);
+clf
+plot(uGAP,pdfGAP,uJKMS,pdfJKMS,uFI,pdfFI,uNI,pdfNI,uIE,pdfIE);
+yline(weightsGAP(:)*u_mat(:));
+title('IE pdf');
+legend('GAP-SQP','JKMS','FI','NI','IE','Location','northwest');
+xlim([umin,0]);
+print(fig,'-dpdf',sprintf('portfolio_figs/%s_IE_pdf_%i.pdf',combo,yN));
 close
 
 end
